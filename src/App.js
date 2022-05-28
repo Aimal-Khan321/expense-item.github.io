8438c16efeb6cf6a7fc04d7efc9a381ac12082a6
Aimal <Aimal.khan02676@gmail.com>
2022-05-29 00:02:13 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpenses/NewExpense';
+import './index.css'
+
+let DUMMY_EXPENSE = [];
 
 function App() {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+
+  function fetchData() {
+    fetch('http://localhost/sample-api/api/read.php')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        // console.log(data);
+        setExpenses(data);
+      })
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const addExpenseData = (expense) => {
+    fetch('localhost/sample-api/api/create.php', {
+      method: 'POST',
+      body: JSON.stringify(expense),
+      headers: {
+        'content-Type': 'application/json'
+      }.then(
+        response => {
+          fetchData();
+        })
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewExpense onAddExpense={addExpenseData} />
+      <Expenses item={expenses} />
+
     </div>
   );
 }
